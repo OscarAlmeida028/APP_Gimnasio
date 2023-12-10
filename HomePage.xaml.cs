4c3e9518c3b17ce84a98cd1c00c059ea43663559
oscaralmeida028@gmail.com <oscaralmeida028@gmail.com>
2023-12-10 11:34:41 -0500

    Se agrega diseño a Home

--- HomePage.xaml.cs
+++ HomePage.xaml.cs
@@ -6,4 +6,24 @@ public partial class HomePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void OnClickHistorialPagos(object sender, EventArgs e)
+    {
+		await Navigation.PushAsync(new HistorialPagoPage());
+    }
+
+    private async void OnClickHistorialVisitas(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new HistorialVisitasPage());
+    }
+
+    private async void OnClickDetallesMembresia(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new DetallesMembresiaPage());
+    }
+
+    private async void OnClikDetallesMiembro(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new DetallesMiembroPage());
+    }
 }
