a56ae5927b081c83a6394229f523414c0de978a4
oscaralmeida028@gmail.com <oscaralmeida028@gmail.com>
2023-12-09 23:46:49 -0500

    Se agrea modelos

---
+++ Models/Miembro.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APP_Gimnasio.Models
+{
+    public class Miembro
+    {
+        [Key]
+        public int idMiembro { get; set; }
+
+        public int idMembresia { get; set; }
+
+        [Required]
+        public string nombreMiembro { get; set; }
+
+        [Required]
+        public string apellidoMiembro { get; set; }
+
+        [Required, EmailAddress]
+        public string emailMiembro { get; set; }
+
+        [Required]
+        public DateTime fechaInscripcion { get; set; }
+    }
+
+}
