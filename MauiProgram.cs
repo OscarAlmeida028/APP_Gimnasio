7a9ac397ba4abe89d6e4bf9dc111cb000ce1f17c
oscaralmeida028@gmail.com <oscaralmeida028@gmail.com>
2023-12-10 11:34:19 -0500

    Se agrega fuente al proyecto

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,6 +13,7 @@ namespace APP_Gimnasio
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("NovaSquare-Regular.ttf", "NovaSquare-Regular");
                 });
 
 #if DEBUG
