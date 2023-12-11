c1824701d108b659116207a6fd5fd2622e30dbeb
oscaralmeida028@gmail.com <oscaralmeida028@gmail.com>
2023-12-11 18:47:55 -0500

    Se finaliza

--- DetallesMiembroPage.xaml.cs
+++ DetallesMiembroPage.xaml.cs
@@ -1,3 +1,5 @@
+using Java.Lang;
+
 namespace APP_Gimnasio;
 
 public partial class DetallesMiembroPage : ContentPage
@@ -8,6 +10,25 @@ public partial class DetallesMiembroPage : ContentPage
 		InitializeComponent();
         _APIService = apiservice;
 	}
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        string idMiembro = Preferences.Get("idMiembro", "0");
+        string nombreMiembro = Preferences.Get("nombre", "0");
+        string apellidoMiembro = Preferences.Get("apellido", "0");
+        string correoMiembro = Preferences.Get("email", "0");
+        string fecha = Preferences.Get("fechaInscripcion", "0");
+
+        // Concatenar el nombre y apellido antes de asignar al label
+
+        string nombreCompleto = $"{nombreMiembro} {apellidoMiembro}";
+
+        idUsuario.Text = idMiembro;
+        NombreMiembro.Text = nombreCompleto;
+        Correo.Text = correoMiembro;
+        Fecha.Text = fecha;
+    }
 
     private async void OnClickCerrarSesion(object sender, EventArgs e)
     {
